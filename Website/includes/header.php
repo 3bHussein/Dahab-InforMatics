d4014f90851102431aabc4e98d553655bc529685
Nourhan-Elnabarwy <anourhan728@gmail.com>
2021-02-17 16:13:52 +0200

    header, footer, index

---
+++ Website/includes/header.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Dahab In4matic</title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="Course Project">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="styles/bootstrap4/bootstrap.min.css">
+<link href="plugins/fontawesome-free-5.0.1/css/fontawesome-all.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/owl.carousel.css">
+<link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/owl.theme.default.css">
+<link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/animate.css">
+<link rel="stylesheet" type="text/css" href="styles/main_styles.css">
+<link rel="stylesheet" type="text/css" href="styles/responsive.css">
+</head>
+<body>
+<div class="super_container">
+
+<header class="header d-flex flex-row">
+<div class="header_content d-flex flex-row align-items-center">
+
+<div class="logo_container">
+<div class="logo">
+<!-- <img src="images/logo.png" alt="">
+ --><span>Dahab In4matic</span>
+</div>
+</div>
+
+<nav class="main_nav_container">
+<div class="main_nav">
+<ul class="main_nav_list">
+<li class="main_nav_item"><a href="index.php">home</a></li>
+<li class="main_nav_item"><a href="#">about us</a></li>
+<li class="main_nav_item"><a href="courses.php">courses</a></li>
+<li class="main_nav_item"><a href="elements.php">elements</a></li>
+<li class="main_nav_item"><a href="news.php">news</a></li>
+<li class="main_nav_item"><a href="contact.php">contact</a></li>
+</ul>
+</div>
+</nav>
+</div>
+<div class="header_side d-flex flex-row justify-content-center align-items-center">
+<img src="images/phone-call.svg" alt="">
+<span>+20 122 240 8980</span>
+</div>
+
+<div class="hamburger_container">
+<i class="fas fa-bars trans_200"></i>
+</div>
+</header>
+
+<div class="menu_container menu_mm">
+
+<div class="menu_close_container">
+<div class="menu_close"></div>
+</div>
+
+<div class="menu_inner menu_mm">
+<div class="menu menu_mm">
+<ul class="menu_list menu_mm">
+<li class="menu_item menu_mm"><a href="index.php">Home</a></li>
+<li class="menu_item menu_mm"><a href="#">About us</a></li>
+<li class="menu_item menu_mm"><a href="courses.php">Courses</a></li>
+<li class="menu_item menu_mm"><a href="elements.php">Elements</a></li>
+<li class="menu_item menu_mm"><a href="news.php">News</a></li>
+<li class="menu_item menu_mm"><a href="contact.php">Contact</a></li>
+</ul>
+
+<div class="menu_social_container menu_mm">
+<ul class="menu_social menu_mm">
+<li class="menu_social_item menu_mm"><a href="#"><i class="fab fa-pinterest"></i></a></li>
+<li class="menu_social_item menu_mm"><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+<li class="menu_social_item menu_mm"><a href="#"><i class="fab fa-instagram"></i></a></li>
+<li class="menu_social_item menu_mm"><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+<li class="menu_social_item menu_mm"><a href="#"><i class="fab fa-twitter"></i></a></li>
+</ul>
+</div>
+<div class="menu_copyright menu_mm">Colorlib All rights reserved</div>
+</div>
+</div>
+</div>
---
+++ Website/index.php
@@ -0,0 +1,256 @@
+<?php
+include 'includes/header.php';
+?>
+
+<div class="home">
+
+<div class="hero_slider_container">
+<div class="hero_slider owl-carousel">
+
+<div class="hero_slide">
+<div class="hero_slide_background" style="background-image:url(images/it1.jpg)"></div>
+<div class="hero_slide_container d-flex flex-column align-items-center justify-content-center">
+<div class="hero_slide_content text-center">
+<h1 data-animation-in="fadeInUp" data-animation-out="animate-out fadeOut">Welcome To <span>Dahab In4matic</span></h1>
+</div>
+</div>
+</div>
+
+<div class="hero_slide">
+<div class="hero_slide_background" style="background-image:url(images/it2.jpg)"></div>
+<div class="hero_slide_container d-flex flex-column align-items-center justify-content-center">
+<div class="hero_slide_content text-center">
+<h1 data-animation-in="fadeInUp" data-animation-out="animate-out fadeOut">Welcome To <span>Dahab In4matic</span></h1>
+</div>
+</div>
+</div>
+
+<div class="hero_slide">
+<div class="hero_slide_background" style="background-image:url(images/it.jpg)"></div>
+<div class="hero_slide_container d-flex flex-column align-items-center justify-content-center">
+<div class="hero_slide_content text-center">
+<h1 data-animation-in="fadeInUp" data-animation-out="animate-out fadeOut">Welcome To <span>Dahab In4matic</span></h1>
+</div>
+</div>
+</div>
+</div>
+<div class="hero_slider_left hero_slider_nav trans_200"><span class="trans_200">prev</span></div>
+<div class="hero_slider_right hero_slider_nav trans_200"><span class="trans_200">next</span></div>
+</div>
+</div>
+
+<div class="services page_section">
+<div class="container">
+<div class="row">
+<div class="col">
+<div class="section_title text-center">
+<h1>E-Learning</h1>
+</div>
+</div>
+</div>
+<div class="row services_row">
+<div class="col-lg-4 service_item text-left d-flex flex-column align-items-start justify-content-start">
+<div class="icon_container d-flex flex-column justify-content-end">
+<img src="images/earth-globe.svg" alt="">
+</div>
+<h3>Online Courses</h3>
+<p>In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor fermentum.</p>
+</div>
+<div class="col-lg-4 service_item text-left d-flex flex-column align-items-start justify-content-start">
+<div class="icon_container d-flex flex-column justify-content-end">
+<img src="images/exam.svg" alt="">
+</div>
+<h3>Indoor Courses</h3>
+<p>In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor fermentum.</p>
+</div>
+<div class="col-lg-4 service_item text-left d-flex flex-column align-items-start justify-content-start">
+<div class="icon_container d-flex flex-column justify-content-end">
+<img src="images/books.svg" alt="">
+</div>
+<h3>Amazing Library</h3>
+<p>In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor fermentum.</p>
+</div>
+<div class="col-lg-4 service_item text-left d-flex flex-column align-items-start justify-content-start">
+<div class="icon_container d-flex flex-column justify-content-end">
+<img src="images/professor.svg" alt="">
+</div>
+<h3>Exceptional Professors</h3>
+<p>In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor fermentum.</p>
+</div>
+<div class="col-lg-4 service_item text-left d-flex flex-column align-items-start justify-content-start">
+<div class="icon_container d-flex flex-column justify-content-end">
+<img src="images/blackboard.svg" alt="">
+</div>
+<h3>Top Programs</h3>
+<p>In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor fermentum.</p>
+</div>
+<div class="col-lg-4 service_item text-left d-flex flex-column align-items-start justify-content-start">
+<div class="icon_container d-flex flex-column justify-content-end">
+<img src="images/mortarboard.svg" alt="">
+</div>
+<h3>Graduate Diploma</h3>
+<p>In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor fermentum.</p>
+</div>
+</div>
+</div>
+</div>
+
+<div class="testimonials page_section">
+
+<div class="testimonials_background_container prlx_parent">
+<div class="testimonials_background prlx" style="background-image:url(images/testimonials_background.jpg)"></div>
+</div>
+<div class="container">
+<div class="row">
+<div class="col">
+<div class="section_title text-center">
+<h1>What Is Our Goal ?</h1>
+</div>
+</div>
+</div>
+<div class="row">
+<div class="col-lg-10 offset-lg-1">
+<div class="testimonials_slider_container">
+
+<div class="owl-carousel owl-theme testimonials_slider">
+
+<div class="owl-item">
+<div class="testimonials_item text-center">
+<div class="quote">“</div>
+<p class="testimonials_text">In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor fermentum.In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor fermentum.</p>
+<div class="testimonial_user">
+<div class="testimonial_image mx-auto">
+<img src="images/testimonials_user.jpg" alt="">
+</div>
+<div class="testimonial_name">james cooper</div>
+<div class="testimonial_title">Graduate Student</div>
+</div>
+</div>
+</div>
+
+<div class="owl-item">
+<div class="testimonials_item text-center">
+ <div class="quote">“</div>
+<p class="testimonials_text">In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor fermentum.In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor fermentum.</p>
+<div class="testimonial_user">
+<div class="testimonial_image mx-auto">
+<img src="images/testimonials_user.jpg" alt="">
+</div>
+<div class="testimonial_name">james cooper</div>
+<div class="testimonial_title">Graduate Student</div>
+</div>
+</div>
+</div>
+
+<div class="owl-item">
+<div class="testimonials_item text-center">
+<div class="quote">“</div>
+<p class="testimonials_text">In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor fermentum.In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor fermentum.</p>
+<div class="testimonial_user">
+<div class="testimonial_image mx-auto">
+<img src="images/testimonials_user.jpg" alt="">
+</div>
+<div class="testimonial_name">james cooper</div>
+<div class="testimonial_title">Graduate Student</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+<div class="events page_section">
+<div class="container">
+<div class="row">
+<div class="col">
+<div class="section_title text-center">
+<h1>Upcoming Events</h1>
+</div>
+</div>
+</div>
+<!-- div class="event_items">
+
+<div class="row event_item">
+<div class="col">
+<div class="row d-flex flex-row align-items-end">
+<div class="col-lg-2 order-lg-1 order-2">
+<div class="event_date d-flex flex-column align-items-center justify-content-center">
+<div class="event_day">07</div>
+<div class="event_month">January</div>
+</div>
+</div>
+<div class="col-lg-6 order-lg-2 order-3">
+<div class="event_content">
+<div class="event_name"><a class="trans_200" href="#">Student Festival</a></div>
+<div class="event_location">Grand Central Park</div>
+<p>In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor.</p>
+ </div>
+</div>
+<div class="col-lg-4 order-lg-3 order-1">
+<div class="event_image">
+<img src="images/event_1.jpg" alt="https://unsplash.com/@theunsteady5">
+</div>
+</div>
+</div>
+</div>
+</div>
+
+<div class="row event_item">
+<div class="col">
+<div class="row d-flex flex-row align-items-end">
+<div class="col-lg-2 order-lg-1 order-2">
+<div class="event_date d-flex flex-column align-items-center justify-content-center">
+<div class="event_day">07</div>
+<div class="event_month">January</div>
+</div>
+</div>
+<div class="col-lg-6 order-lg-2 order-3">
+<div class="event_content">
+<div class="event_name"><a class="trans_200" href="#">Open day in the Univesrsity campus</a></div>
+<div class="event_location">Grand Central Park</div>
+<p>In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor.</p>
+</div>
+</div>
+<div class="col-lg-4 order-lg-3 order-1">
+<div class="event_image">
+<img src="images/event_2.jpg" alt="https://unsplash.com/@claybanks1989">
+</div>
+</div>
+</div>
+</div>
+</div>
+
+<div class="row event_item">
+<div class="col">
+<div class="row d-flex flex-row align-items-end">
+<div class="col-lg-2 order-lg-1 order-2">
+<div class="event_date d-flex flex-column align-items-center justify-content-center">
+<div class="event_day">07</div>
+<div class="event_month">January</div>
+</div>
+</div>
+<div class="col-lg-6 order-lg-2 order-3">
+<div class="event_content">
+<div class="event_name"><a class="trans_200" href="#">Student Graduation Ceremony</a></div>
+<div class="event_location">Grand Central Park</div>
+<p>In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor.</p>
+</div>
+</div>
+<div class="col-lg-4 order-lg-3 order-1">
+<div class="event_image">
+<img src="images/event_3.jpg" alt="https://unsplash.com/@juanmramosjr">
+</div>
+</div>
+</div>
+</div>
+</div>
+</div> -->
+</div>
+</div>
+
+<?php
+include 'includes/footer.php';
+?>
