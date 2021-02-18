cbfdbbe24dda721cc5cc2cbb7f623caf778f5ef7
3bHussein <39864308+3bHussein@users.noreply.github.com>
2021-02-18 09:59:39 +0200

    Edit About Page

---
+++ Website/aboutus.php
@@ -0,0 +1,278 @@
+<link rel="stylesheet" type="text/css" href="styles/news_post_styles.css">
+<link rel="stylesheet" type="text/css" href="styles/news_post_responsive.css">
+<style>
+  .icon_boxes{margin-top:112px}.icon_boxes_container{margin-top:109px}.icon_box{}.icon_box h3{font-family:roboto,sans-serif;font-size:22px;font-weight:500;color:#1a1a1a;margin-bottom:13px}.icon_box p{font-size:14px;font-weight:500;color:#a5a5a5;max-width:100%;margin-bottom:0}.icon_container{height:41px;width:auto;margin-bottom:30px}.icon_container img{height:100%}
+  </style>
+<?php
+include 'includes/header.php';
+?>
+
+
+<!------------------------------------------------------------------------------------------------>
+<!-- End of Header -->
+
+
+
+<!-- Start About page -->
+
+<div class="home">
+<div class="home_background_container prlx_parent">
+<div class="home_background prlx" style="background-image:url(images/news_background.jpg)"></div>
+</div>
+<div class="home_content">
+<h1>About Us </h1>
+</div>
+</div>
+
+<div class="news">
+<div class="container">
+
+
+
+
+<div class="row">
+<div class="col">
+<div class="section_title text-center">
+<h1>Our mission</h1>
+</div>
+</div>
+</div>
+
+
+ 
+<div class="news_post_quote">
+<p class="news_post_quote_text"><span>W</span>e enable progressive businesses to transform, scale and gain competitive advantage, through the expert delivery of innovative, tailor-made software. Our elegant, data-driven 
+solutions help organizations and people around the world to perform more effectively and achieve better outcomes.
+</p>
+</div>
+
+      
+  
+  <!-- <div class="col-lg-8"> -->
+    <!-- <div class="col-lg-12">
+      <div class="news_post_container">
+
+<div class="news_post">
+<div class="news_post_image">
+<img src="images/news_1.jpg" alt="https://unsplash.com/@dsmacinnes">
+</div>
+
+<div class="news_post_top d-flex flex-column flex-sm-row">
+<div class="news_post_date_container">
+<div class="news_post_date d-flex flex-column align-items-center justify-content-center">
+<div>18</div>
+<div>dec</div>
+</div>
+</div>
+<div class="news_post_title_container">
+<div class="news_post_title">
+<a href="news_post.html">Why do you need a qualification?</a>
+</div>
+<div class="news_post_meta">
+<span class="news_post_author"><a href="#">By Christian Smith</a></span>
+<span>|</span>
+<span class="news_post_comments"><a href="#">3 Comments</a></span>
+</div>
+</div>
+</div>
+<div class="news_post_text">
+<p>In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor fermentum. Etiam eu purus nec eros varius luctus. Praesent finibus risus facilisis ultricies. Etiam eu purus nec eros varius luctus. Praesent finibus risus facilisis ultricies venenatis. Suspendisse fermentum sodales lacus, lacinia gravida elit dapibus sed. Cras in lectus elit. Maecenas tempus nunc vitae mi egestas venenatis. Aliquam rhoncus, purus in vehicula porttitor, lacus ante consequat purus, id elementum enim purus nec enim. In sed odio rhoncus, tristique ipsum id, pharetra neque. </p>
+</div>
+
+
+<div class="news_post_quote">
+<p class="news_post_quote_text"><span>E</span>tiam eu purus nec eros varius luctus. Praesent finibus risus facilisis ultricies venena tis. Suspendisse fermentum sodales lacus, lacinia gravida elit.</p>
+</div>
+
+
+<p class="news_post_text" style="margin-top: 59px;">In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor fermentum. Etiam eu purus nec eros varius luctus. Praesent finibus risus facilisis ultricies. Etiam eu purus nec eros varius luctus. Praesent finibus risus facilisis ultricies venenatis. Suspendisse fermentum sodales lacus, lacinia gravida elit dapibus sed. Cras in lectus elit. Maecenas tempus nunc vitae mi egestas venenatis. Aliquam rhoncus, purus in vehicula porttitor, lacus ante consequat purus, id elementum enim purus nec enim. In sed odio rhoncus, tristique ipsum id, pharetra neque. </p>
+</div>
+</div>
+
+<div class="news_post_comments">
+<div class="comments_title">Comments</div>
+<ul class="comments_list">
+
+<li class="comment">
+<div class="comment_container d-flex flex-row">
+<div>
+<div class="comment_image">
+<img src="images/comment_1.jpg" alt="">
+</div>
+</div>
+<div class="comment_content">
+<div class="comment_meta">
+<span class="comment_name"><a href="#">Mark Smith</a></span>
+<span class="comment_separator">|</span>
+<span class="comment_date">Dec 18, 2017</span>
+<span class="comment_separator">|</span>
+<span class="comment_reply_link"><a href="#">Reply</a></span>
+</div>
+<p class="comment_text">Aliquam rhoncus, purus in vehicula porttitor, lacus ante consequat purus, id elementum enim purus nec enim. In sed odio rhoncus, tristique ipsum id, pharetra neque. </p>
+</div>
+</div>
+</li>
+
+<li class="comment">
+<div class="comment_container d-flex flex-row">
+<div>
+<div class="comment_image">
+<img src="images/comment_2.jpg" alt="">
+</div>
+</div>
+<div class="comment_content">
+<div class="comment_meta">
+<span class="comment_name"><a href="#">Mark Smith</a></span>
+<span class="comment_separator">|</span>
+<span class="comment_date">Dec 18, 2017</span>
+<span class="comment_separator">|</span>
+<span class="comment_reply_link"><a href="#">Reply</a></span>
+</div>
+<p class="comment_text">Aliquam rhoncus, purus in vehicula porttitor, lacus ante consequat purus, id elementum enim purus nec enim. In sed odio rhoncus, tristique ipsum id, pharetra neque. </p>
+</div>
+</div>
+</li>
+</ul>
+</div>
+
+<!-- <div class="leave_comment">
+<div class="leave_comment_title">Leave a comment</div>
+<div class="leave_comment_form_container">
+<form action="post">
+<input id="comment_form_name" class="input_field contact_form_name" type="text" placeholder="Name" required="required" data-error="Name is required.">
+<input id="comment_form_email" class="input_field contact_form_email" type="email" placeholder="E-mail" required="required" data-error="Valid email is required.">
+<textarea id="comment_form_message" class="text_field contact_form_message" name="message" placeholder="Message" required="required" data-error="Please, write us a message."></textarea>
+<button id="comment_send_btn" type="submit" class="comment_send_btn trans_200" value="Submit">send message</button>
+</form>
+</div>
+</div> 
+</div> -->
+<!-- 
+<div class="col-lg-4">
+<div class="sidebar">
+
+<div class="sidebar_section">
+<div class="sidebar_section_title">
+<h3>Archives</h3>
+</div>
+<ul class="sidebar_list">
+<li class="sidebar_list_item"><a href="#">Design Courses</a></li>
+<li class="sidebar_list_item"><a href="#">All you need to know</a></li>
+<li class="sidebar_list_item"><a href="#">Uncategorized</a></li>
+<li class="sidebar_list_item"><a href="#">About Our Departments</a></li>
+<li class="sidebar_list_item"><a href="#">Choose the right course</a></li>
+</ul>
+</div> -->
+<!--
+<div class="sidebar_section">
+<div class="sidebar_section_title">
+<h3>Latest posts</h3>
+</div>
+<div class="latest_posts">
+
+ <div class="latest_post">
+<div class="latest_post_image">
+<img src="images/latest_1.jpg" alt="https://unsplash.com/@dsmacinnes">
+</div>
+<div class="latest_post_title"><a href="news_post.html">Why do you need a qualification?</a></div>
+<div class="latest_post_meta">
+<span class="latest_post_author"><a href="#">By Christian Smith</a></span>
+<span>|</span>
+<span class="latest_post_comments"><a href="#">3 Comments</a></span>
+</div>
+</div> -->
+<!-- 
+<div class="latest_post">
+<div class="latest_post_image">
+<img src="images/latest_2.jpg" alt="https://unsplash.com/@erothermel">
+</div>
+<div class="latest_post_title"><a href="news_post.html">Why do you need a qualification?</a></div>
+<div class="latest_post_meta">
+<span class="latest_post_author"><a href="#">By Christian Smith</a></span>
+<span>|</span>
+<span class="latest_post_comments"><a href="#">3 Comments</a></span>
+</div>
+</div> -->
+
+<!-- <div class="latest_post">
+<div class="latest_post_image">
+<img src="images/latest_3.jpg" alt="https://unsplash.com/@element5digital">
+</div>
+<div class="latest_post_title"><a href="news_post.html">Why do you need a qualification?</a></div>
+<div class="latest_post_meta">
+<span class="latest_post_author"><a href="#">By Christian Smith</a></span>
+<span>|</span>
+<span class="latest_post_comments"><a href="#">3 Comments</a></span>
+</div>
+</div>
+
+
+</div>
+</div> -->
+<!-- 
+<div class="sidebar_section">
+<div class="sidebar_section_title">
+<h3>Tags</h3>
+</div>
+<div class="tags d-flex flex-row flex-wrap">
+<div class="tag"><a href="#">Course</a></div>
+<div class="tag"><a href="#">Design</a></div>
+<div class="tag"><a href="#">FAQ</a></div>
+<div class="tag"><a href="#">Teachers</a></div>
+<div class="tag"><a href="#">School</a></div>
+<div class="tag"><a href="#">Graduate</a></div>
+</div>
+</div> 
+</div>
+</div>-->
+
+<div class="icon_boxes">
+<div class="container">
+<div class="row">
+<div class="col">
+<div class="elements_title">Icon Boxes</div>
+</div>
+</div>
+<div class="row icon_boxes_container">
+<div class="col-lg-4 icon_box text-left d-flex flex-column align-items-start justify-content-start">
+<div class="icon_container d-flex flex-column justify-content-end">
+<img src="images/earth-globe.svg" alt="">
+</div>
+<h3>Online Courses</h3>
+<p>In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor fermentum.</p>
+</div>
+<div class="col-lg-4 icon_box text-left d-flex flex-column align-items-start justify-content-start">
+<div class="icon_container d-flex flex-column justify-content-end">
+<img src="images/exam.svg" alt="">
+</div>
+<h3>Indoor Courses</h3>
+<p>In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor fermentum.</p>
+</div>
+<div class="col-lg-4 icon_box text-left d-flex flex-column align-items-start justify-content-start">
+<div class="icon_container d-flex flex-column justify-content-end">
+<img src="images/books.svg" alt="">
+</div>
+<h3>Amazing Library</h3>
+<p>In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor fermentum.</p>
+</div>
+</div>
+</div>
+</div>
+
+
+</div>
+</div>
+</div>
+
+
+
+
+
+ 
+
+<!-- Start Footer Section  -->
+
+
+<?php
+include 'includes/footer.php';
+?>
