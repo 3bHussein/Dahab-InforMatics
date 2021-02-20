423484e72bc90e89390e571fa077a6544d6cdb2a
3bHussein <39864308+3bHussein@users.noreply.github.com>
2021-02-20 02:25:48 +0200

    Finish Design for About Us
    -Add Map photo

--- Website/aboutus.php
+++ Website/aboutus.php
@@ -230,41 +230,152 @@ solutions help organizations and people around the world to perform more effecti
 <div class="container">
 <div class="row">
 <div class="col">
-<div class="elements_title">Icon Boxes</div>
+
+
+<!--Why Us Header  -->
+<div class="row">
+<div class="col">
+<div class="section_title text-center">
+<h1>Why Choose Us</h1>
+</div>
+</div>
+</div>
+<!--  -->
+ 
+<!-- <div class="elements_title">Icon Boxes</div> -->
 </div>
 </div>
 <div class="row icon_boxes_container">
 <div class="col-lg-4 icon_box text-left d-flex flex-column align-items-start justify-content-start">
 <div class="icon_container d-flex flex-column justify-content-end">
-<img src="images/earth-globe.svg" alt="">
+<!-- <img src="images/earth-globe.svg" alt=""> -->
+<img  src="images/user experience.svg" alt="">
 </div>
-<h3>Online Courses</h3>
-<p>In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor fermentum.</p>
+<h3 >We Create Experiences</h3>
+<p>
+We create experiences that are attractive, simple to use, and drive results for your company. We are not your
+ typical web development agency.
+  <!-- Sure, we’re strong on corporate branding and web design,
+  but we’re really focused on making things work for your audience ... and your business. -->
+
+
+</p>
 </div>
 <div class="col-lg-4 icon_box text-left d-flex flex-column align-items-start justify-content-start">
 <div class="icon_container d-flex flex-column justify-content-end">
 <img src="images/exam.svg" alt="">
 </div>
-<h3>Indoor Courses</h3>
-<p>In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor fermentum.</p>
+<h3>We Ask, Listen and Understand</h3>
+<p>
+We begin each web development project by gaining a solid understanding of who you are, what you do and why you do it.
+ <!-- This helps us create
+ strategic web design solutions that communicate the right messages and express your unique brand identity. -->
+</p>
 </div>
 <div class="col-lg-4 icon_box text-left d-flex flex-column align-items-start justify-content-start">
 <div class="icon_container d-flex flex-column justify-content-end">
 <img src="images/books.svg" alt="">
 </div>
-<h3>Amazing Library</h3>
-<p>In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor fermentum.</p>
+<h3>We Do The Work</h3>
+<p>
+From principals to junior members of the team, we all work together on website development projects. 
+You will see the same faces from the first hello to the completion of the project.
+</p>
 </div>
 </div>
 </div>
 </div>
 
 
+
+
+
+
+
+
+
 </div>
 </div>
 </div>
 
+  
+<!-- Our Goals Section -->
+<div class="testimonials page_section">
 
+<div class="testimonials_background_container prlx_parent">
+<!-- <div class="testimonials_background prlx" style="background-image:url(images/testimonials_background.jpg)"></div> -->
+<div class="testimonials_background prlx" style="background-image:url(images/map.jpg)"></div>
+</div>
+<div class="container">
+<div class="row">
+<div class="col">
+<div class="section_title text-center">
+<h1>Our Goals</h1>
+</div>
+</div>
+</div>
+<div class="row">
+<div class="col-lg-10 offset-lg-1">
+<div class="testimonials_slider_container">
+
+<!-- <div class="owl-carousel owl-theme testimonials_slider"> -->
+<!-- <div class="owl-carousel owl-theme testimonials_slider"> -->
+
+<!-- <div class="owl-item"> -->
+<div class="">
+<div class="testimonials_item text-center">
+<div class="quote">“</div>
+<p class="testimonials_text"> 
+  <!-- a gravida rutrum, ante nisl fermentum nulla, v
+  itae tempor nisl ligula vel nunc. Proin quis mi males
+  uada, finibus tortor fermentum.In aliquam, augue a gravida 
+  rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel 
+  nunc. Proin quis mi malesuada, finibus tortor fermentum. -->
+<!-- 
+  “To become the number 1 Tech Comapany   globally.”
+
+
+  global trading platform where practically anyone can trade practically anything.”
+  
+
+  To Be a Trading global Platform -->
+
+  “ Our goal is to become a global, efficient, high-quality software company. ”
+
+</p>
+<div class="testimonial_user">
+<div class="testimonial_image mx-auto">
+<img src="images/testimonials_user.jpg" alt="">
+</div>
+<div class="testimonial_name">Dr.Aly Mahgob</div>
+<div class="testimonial_title">Co-founder</div>
+</div>
+</div>
+</div>
+
+<!-- <div class="owl-item">
+<div class="testimonials_item text-center">
+ <div class="quote">“</div>
+<p class="testimonials_text">In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor fermentum.In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor fermentum.</p>
+<div class="testimonial_user">
+<div class="testimonial_image mx-auto">
+<img src="images/testimonials_user.jpg" alt="">
+</div>
+<div class="testimonial_name">james cooper</div>
+<div class="testimonial_title">Graduate Student</div>
+</div>
+</div>
+</div> -->
+
+
+<!-- </div> -->
+</div>
+</div>
+</div>
+</div>
+</div>
+
+<!--  -->
 
 
 
