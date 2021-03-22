57726fa8f0eb6108776b9bb6abf0f23448355ec0
Yehia Abdelbasier <yehiafml20333@gmail.com>
2021-03-22 14:12:31 +0200

    Create oldmaster.blade.php

---
+++ Dahab Website/oldmaster.blade.php
@@ -0,0 +1,292 @@
+<?php
+include 'includes/header.php';
+?>
+
+<!-- Style for Ourtechnology Section -->
+<!-- Maybe Make Error with Responsive -->
+<style>
+    .design-services .design-items {
+        text-align: center;
+        -js-display: flex;
+        display: -webkit-box;
+        display: -ms-flexbox;
+        display: flex;
+        -webkit-box-pack: justify;
+        -ms-flex-pack: justify;
+        justify-content: space-between;
+        font-family: 'Roboto', sans-serif;
+        font-size: 22px;
+        font-weight: 500;
+        color: #1a1a1a;
+        margin-bottom: 13px;
+        
+    }
+    .h33{
+        font-family: 'Roboto', sans-serif;
+    font-size: 22px;
+    font-weight: 500;
+    color: #1a1a1a;
+    margin-bottom: 13px;
+    }
+
+    .button_line_2 {
+    background: 0 0;
+    border: solid 2px #1a1a1a;
+        }
+
+        .button1 {
+    display: inline-block;
+    width: 188px;
+    height: 53px;
+    vertical-align: middle;
+    margin-right: 9px;
+    margin-bottom: 15px;
+        }
+        .button_line_2 a {
+    color: #1a1a1a;
+}
+
+.button1 a {
+    line-height: 53px;
+    display: block;
+    font-size: 16px;
+    font-weight: 700;
+}
+</style>
+
+<div class="home">
+    <!-- Slider section -->
+    <div class="hero_slider_container">
+        <div class="hero_slider owl-carousel">
+
+            <div class="hero_slide">
+                <div class="hero_slide_background" style="background-image:url(images/home.jpeg)"></div>
+                <!--<div class="hero_slide_background" style="background-image:url(images/background.jpg)"></div>-->
+                <!--<div class="hero_slide_background" style="background-image:url(images/it1.jpg)"></div>-->
+                <div class="hero_slide_container d-flex flex-column align-items-center justify-content-center">
+                    <div class="hero_slide_content text-center">
+                        <h1 data-animation-in="fadeInUp" data-animation-out="animate-out fadeOut">Welcome To <span>Dahab Informatics</span></h1>
+                        
+                        <h2 data-animation-in="fadeInUp" data-animation-out="animate-out fadeOut"style="color:#FFFF; padding-top : 10%;" class="">
+                            E-Learing Tools </h2>
+                    </div>
+                </div>
+            </div>
+             <div class="hero_slide">
+                <!--<div class="hero_slide_background" style="background-image:url(images/e-l.jpg)"></div>-->
+                <div class="hero_slide_background" style="background-image:url(images/e-l.jpg)"></div>
+                <div class="hero_slide_container d-flex flex-column align-items-center justify-content-center">
+                    <div class="hero_slide_content text-center">
+                        <h1 data-animation-in="fadeInUp" data-animation-out="animate-out fadeOut">
+                            Developing <span>E-learning</span> <span>tools</span></h1>
+                    </div>
+                </div>
+            </div>
+
+            <div class="hero_slide">
+                <div class="hero_slide_background" style="background-image:url(images/web5.jpg)"></div>
+                <!--<div class="hero_slide_background" style="background-image:url(images/bb.jpg)"></div>-->
+                <div class="hero_slide_container d-flex flex-column align-items-center justify-content-center">
+                    <div class="hero_slide_content text-center">
+                        <h1 data-animation-in="fadeInUp" data-animation-out="animate-out fadeOut">
+                            Developing <span>Web</span> <span>  Applications</span></h1>
+                    </div>
+                </div>
+            </div>
+
+            <div class="hero_slide">
+                <div class="hero_slide_background" style="background-image:url(images/and.jpg)"></div>
+                <!--<div class="hero_slide_background" style="background-image:url(images/it.jpg)"></div>-->
+                <div class="hero_slide_container d-flex flex-column align-items-center justify-content-center">
+                    <div class="hero_slide_content text-center">
+                        <h1 data-animation-in="fadeInUp" data-animation-out="animate-out fadeOut"> Developing <span>Android</span>
+                            <span>Applications</span></h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Remove slider -->
+        <!-- <div class="hero_slider_left hero_slider_nav trans_200"><span class="trans_200">prev</span></div>
+        <div class="hero_slider_right hero_slider_nav trans_200"><span class="trans_200">next</span></div> -->
+    </div>
+</div>
+
+<!-- Services section -->
+<div class="services page_section">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="section_title text-center">
+                    <h1>Software Development Company</h1>
+                    <p style="font-size: 15px; color: #333333;">We are a dynamic team of skilled developers, designers, and support experts dedicated to providing the web solution you need to achieve success.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="row services_row">
+
+            <div class="col-lg-12 service_item text-left d-flex flex-column align-items-start justify-content-start">
+                <div class="icon_container d-flex flex-column justify-content-end">
+                    <img src="images/elearning.png" alt="">
+                </div>
+                <h3>E-Learning Platforms</h3>
+                <p>Designed to be responsive and accessible for students, the platform interface is easy to navigate on both desktop and mobile devices.</p>
+            </div>
+
+            <div class="col-lg-6 service_item text-left d-flex flex-column align-items-start justify-content-start">
+                <div class="icon_container d-flex flex-column justify-content-end">
+                    <img src="images/web.png" alt="">
+                </div>
+                <h3>Web Applications Development</h3>
+                <p>Designing and developing high quality scalable web applications.</p>
+            </div>
+
+            <div class="col-lg-6 service_item text-left d-flex flex-column align-items-start justify-content-start">
+                <div class="icon_container d-flex flex-column justify-content-end">
+                    <img src="images/mobile.png" alt="">
+                </div>
+                <h3>Mobile Applications Development</h3>
+                <p>Building and designing an optimized and native android applications.</p>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+
+
+
+
+<!-- ourtechnology  Section -->
+<!-- dimension for logo 77*62 -->
+<section class="design-services">
+    <div class="container">
+
+        <div class="section_title text-center">
+            <h1>Our Technologies</h1>
+            <p style="font-size: 15px; color: #333333;">Our software engineers have expertise in all the technologies needed to deliver comprehensive web developments services for companies across industries.</p>
+        </div>
+
+
+
+        <!-- <h2 style="text-align: center;">Our Technologies</h2>
+        <p class="solutions-page__descr" style="text-align: center; margin-bottom: 30px;">
+        <span style="font-weight: 400;">Our software engineers have expertise in all the technologies needed to deliver comprehensive web developments services for companies across industries.</span>
+        </p> -->
+        <p>&nbsp;</p>
+        <ul class="design-items form-row col">
+
+
+            <li class="design-item">
+                <div class="design-icon">
+                    <img class="offer-image" src="images/ourtechnology/php.svg" alt="PHP technology" style="width: 59px;height: 62px;">
+                </div>
+                <h3 class="design-title">PHP</h3>
+            </li>
+            <li class="design-item">
+                <div class="design-icon">
+                    <img class="offer-image" src="images/ourtechnology/.netcore.svg"alt="C# technology" style="width: 59px;height: 62px;">
+                </div>
+                <h3 class="design-title">C#</h3>
+            </li>
+
+            <li class="design-item">
+                <div class="design-icon">
+                    <img class="offer-image" src="images/ourtechnology/flutter.svg" alt="Flutter technology" style="width: 59px;height: 60px;">
+                </div>
+                <h3 class="design-title">Flutter</h3>
+            </li>
+            <li class="design-item">
+                <div class="design-icon">
+                    <img class="offer-image" src="images/ourtechnology/js.svg"alt="JS technology" style="width: 59px;height: 62px;" >
+                </div>
+                <h3 class="design-title">JavaScript</h3>
+            </li>
+
+            <li class="design-item">
+                <div class="design-icon">
+                    <img class="offer-image" src="images/ourtechnology/jquery.svg" alt="JQuery library"style="width: 59px;height: 62px;">
+                </div>
+                <h3 class="design-title">jQuery</h3>
+            </li>
+        </ul>
+    </div>
+</section>
+
+
+
+<!-- End ourtechnology  Section -->
+
+<!-- Events Section -->
+<div class="events page_section">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="section_title text-center">
+                    <h1>Upcoming Events</h1>
+                    <!-- <div style="background-color: #fcb406"><img src="images/tail-spin.svg"></div> -->
+                    
+
+                    <!-- <div style="  width: 100px;  height: 100px; margin: 20px; display:inline-block;">
+                            <svg version="1.1" id="L9" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                                                        viewBox="0 0 100 100" enable-background="new 0 0 0 0" xml:space="preserve">
+                                <path fill="#fcb406" d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50">
+                                    <animateTransform
+                                        attributeName="transform"
+                                        attributeType="XML"
+                                        type="rotate"
+                                        dur="1s"
+                                        from="0 50 50"
+                                        to="360 50 50"
+                                        repeatCount="indefinite" />
+                                </path>
+                            </svg>
+                    </div> -->
+                    <!-- <div style="color: black; font-family: 'Roboto', sans-serif;font-size: 22px;font-weight: 500;">
+                        Co<strong style="color:#fcb406">mm</strong>ing S<strong style="color:#fcb406">oo</strong>n
+                    </div> -->
+                </div>
+            </div>
+        </div>
+        
+        <div class="event_items">
+
+     
+
+        <div class="row event_item">
+        <div class="col">
+        <div class="row d-flex flex-row align-items-end">
+        <!-- <div class="col-lg-2 order-lg-1 order-2">
+        <div class="event_date d-flex flex-column align-items-center justify-content-center">
+        <div class="event_day">07</div>
+        <div class="event_month">January</div>
+        </div>
+        </div> -->
+        <div class="col-lg-6 order-lg-2 order-3">
+        <div class="event_content">
+        <div class="event_name">
+            <!-- <a class="trans_200" href="#">First Online Education</a> -->
+            <h3 class="h33">First Online Education</h3>
+          </div>
+        <!-- <div class="event_location">Grand Central Park</div> -->
+        <p>First Online Education platform.</p>
+        <div class="button1 button_line_2 text-center trans_200"><a href="https://egy-x.com/" target="_blank">Read More</a></div>
+        </div>
+        </div>
+        <div class="col-lg-4 order-lg-3 order-1">
+        <div class="event_image">
+        <img src="images/e-learning.jfif" alt="E-learning platform">
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
+
+         
+        </div> 
+    </div>
+</div>
+
+<?php
+include 'includes/footer.php';
+?>
